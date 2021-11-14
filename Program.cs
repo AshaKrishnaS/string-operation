c910d96a206a3f5038f4ef396d62b83a269404ad
Asha Krishna S <68347359+AshaKrishnaS@users.noreply.github.com>
2021-11-14 20:35:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace stringoperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fname = "asha";
+            string lname = "krishna";
+            //finding length
+            Console.WriteLine(fname.Length);
+            //concatination
+            string name = fname + lname;
+            Console.WriteLine(name);
+            //interpolation
+            string name1=$"My full name is :{ fname} { lname}";
+            Console.WriteLine(name1);
+
+        }
+    }
+}
